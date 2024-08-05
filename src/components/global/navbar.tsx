cbbd607e5e003d0c5247aa339e01e5d0c73a519f
Danilo Martinelli <danilomartinelli429@gmail.com>
2024-08-05 16:11:24 -0300

    feat: :lipstick: Add CTA navbar button
    Add a Dashboard/Get Started button and a menu icon

--- src/components/global/navbar.tsx
+++ src/components/global/navbar.tsx
@@ -1,3 +1,4 @@
+import { MenuIcon } from 'lucide-react';
 import Link from 'next/link';
 
 interface INavbarProps {}
@@ -30,6 +31,20 @@ const Navbar = async (props: INavbarProps) => {
           </li>
         </ul>
       </nav>
+      <aside className="flex items-center gap-4">
+        <Link
+          href="/dashboard"
+          className="relative inline-flex h-10 overflow-hidden rounded-full p-[2px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
+        >
+          <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+          <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+            {/* TODO: Wire up user  */}
+            {true ? 'Dashboard' : 'Get Started'}
+          </span>
+        </Link>
+        {/* TODO: Wire up user */}
+        <MenuIcon className="md:hidden" />
+      </aside>
     </header>
   );
 };
