caac73f57bf200ae1108e1cdc4a3b37a1faaae24
Danilo Martinelli <danilomartinelli429@gmail.com>
2024-08-06 11:54:03 -0300

    feat: :construction: Add a Lamp Effect
    Create a custom component to do a lamp effect in the Landing Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { HeroParallax } from '@/components/global/connect-parallax';
 import { ContainerScroll } from '@/components/global/container-scroll-animation';
 import { InfiniteMovingCards } from '@/components/global/infinite-moving-cards';
+import { LampComponent } from '@/components/global/lamp';
 import Navbar from '@/components/global/navbar';
 import { Button } from '@/components/ui/button';
 import { clients, products } from '@/lib/constants';
@@ -40,6 +41,9 @@ export default function Home() {
       />
       <section>
         <HeroParallax products={products}></HeroParallax>
+      </section>
+      <section className="mt-[-500px]">
+        <LampComponent />
       </section>
     </main>
   );
