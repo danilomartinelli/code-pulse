dfc4a680c245f67dece882ec2036fc8ae9fc55e4
Danilo Martinelli <danilomartinelli429@gmail.com>
2024-08-06 21:38:17 -0300

    feat: :lipstick: Create non-lp layouts
    Create a layout to a group called main and a group called pages

---
+++ src/app/(main)/layout.tsx
@@ -0,0 +1,18 @@
+// import Sidebar from '@/components/sidebar';
+// import InfoBar from '@/components/infobar';
+
+type Props = { children: React.ReactNode };
+
+const Layout = (props: Props) => {
+  return (
+    <div className="flex overflow-hidden h-screen">
+      {/* <Sidebar /> */}
+      <div className="w-full">
+        {/* <InfoBar /> */}
+        {props.children}
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
