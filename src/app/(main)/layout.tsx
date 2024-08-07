aa60de1cb2ac89915e38f62b553754465654ff54
Danilo Martinelli <danilomartinelli429@gmail.com>
2024-08-07 08:10:09 -0300

    feat: :lipstick: Create a Sidebar
    Add the Sidebar component at the main group of pages layout.

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,4 +1,4 @@
-// import Sidebar from '@/components/sidebar';
+import Sidebar from '@/components/sidebar';
 // import InfoBar from '@/components/infobar';
 
 type Props = { children: React.ReactNode };
@@ -6,7 +6,7 @@ type Props = { children: React.ReactNode };
 const Layout = (props: Props) => {
   return (
     <div className="flex overflow-hidden h-screen">
-      {/* <Sidebar /> */}
+      <Sidebar />
       <div className="w-full">
         {/* <InfoBar /> */}
         {props.children}
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,11 @@
+import Category from '@/components/icons/category';
+import Logs from '@/components/icons/clipboard';
+import Templates from '@/components/icons/cloud_download';
+import Home from '@/components/icons/home';
+import Payment from '@/components/icons/payment';
+import Settings from '@/components/icons/settings';
+import Workflows from '@/components/icons/workflows';
+
 export const clients = [...new Array(10)].map((client, index) => ({
   href: `/${index + 1}.png`,
 }));
@@ -81,4 +89,14 @@ export const products = [
     link: 'https://efreeinvoice.com',
     thumbnail: '/p3.png',
   },
+];
+
+export const menuOptions = [
+  { name: 'Dashboard', Component: Home, href: '/dashboard' },
+  { name: 'Workflows', Component: Workflows, href: '/workflows' },
+  { name: 'Settings', Component: Settings, href: '/settings' },
+  { name: 'Connections', Component: Category, href: '/connections' },
+  { name: 'Billing', Component: Payment, href: '/billing' },
+  { name: 'Templates', Component: Templates, href: '/templates' },
+  { name: 'Logs', Component: Logs, href: '/logs' },
 ];
