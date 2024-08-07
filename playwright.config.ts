c1c3568f13966824ee3e6c0ffcefcf98ae235a18
Danilo Martinelli <danilomartinelli429@gmail.com>
2024-08-07 19:17:51 -0300

    chore: :heavy_plus_sign: Add playwright
    Install playwright package and create a new test config file

---
+++ playwright.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: 'html',
+  use: {
+    baseURL: 'https://localhost:3000',
+    trace: 'on-first-retry',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+  webServer: {
+    command: 'bun run dev',
+    port: 3000,
+    reuseExistingServer: !process.env.CI,
+  },
+});
