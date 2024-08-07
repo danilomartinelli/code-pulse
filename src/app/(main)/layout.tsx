7da51ef2a0211bf050516350beb6726dd907f17e
Danilo Martinelli <danilomartinelli429@gmail.com>
2024-08-07 10:34:21 -0300

    feat: :lipstick: Create Infobar component
    Create and add the infobar component at the main layout

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,5 +1,5 @@
 import Sidebar from '@/components/sidebar';
-// import InfoBar from '@/components/infobar';
+import InfoBar from '@/components/infobar';
 
 type Props = { children: React.ReactNode };
 
@@ -8,7 +8,7 @@ const Layout = (props: Props) => {
     <div className="flex overflow-hidden h-screen">
       <Sidebar />
       <div className="w-full">
-        {/* <InfoBar /> */}
+        <InfoBar />
         {props.children}
       </div>
     </div>
---
+++ src/components/infobar/index.tsx
@@ -0,0 +1,63 @@
+'use client';
+
+import { Book, Headphones, Search } from 'lucide-react';
+import { Input } from '@/components/ui/input';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/components/ui/tooltip';
+
+const InfoBar = () => {
+  const tier = 'Free'; // TODO: Get tier from API
+  const credits = 5; // TODO: Get credits from API
+
+  return (
+    <div className="flex flex-row justify-end gap-6 items-center px-4 py-4 w-full dark:bg-black ">
+      <span className="flex items-center gap-2 font-bold">
+        <p className="text-sm font-light text-gray-300">Credits</p>
+        {/* TODO: Remember to remove 'ts-ignore' when the tier is no longer a mock */}
+        {/* @ts-ignore */}
+        {tier !== 'Unlimited' ? (
+          <span>
+            {credits}/{tier == 'Free' ? '10' : tier == 'Pro' && '100'}
+          </span>
+        ) : (
+          <span>Unlimited</span>
+        )}
+      </span>
+      <span className="flex items-center rounded-full bg-muted px-4">
+        <Search />
+        <Input
+          placeholder="Quick Search"
+          className="border-none bg-transparent"
+        />
+      </span>
+      <TooltipProvider>
+        <Tooltip delayDuration={0}>
+          <TooltipTrigger>
+            <Headphones />
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>Contact Support</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+      <TooltipProvider>
+        <Tooltip delayDuration={0}>
+          <TooltipTrigger>
+            <Book />
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>Guide</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+      {/* TODO: Add a UserButton component from the Clerk SDK */}
+      {/* <UserButton /> */}
+    </div>
+  );
+};
+
+export default InfoBar;
