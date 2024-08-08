da07e1fed5b48a7b1c38f5d8e67938864d109a0a
Danilo Martinelli <danilomartinelli429@gmail.com>
2024-08-07 22:08:44 -0300

    refactor: :pencil2: Change named function to arrow function
    Change the function sign to a arrow function to apply the react types

--- src/providers/theme-provider.tsx
+++ src/providers/theme-provider.tsx
@@ -3,11 +3,9 @@
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
 import { type ThemeProviderProps } from 'next-themes/dist/types';
 
-export function ThemeProvider({
+export const ThemeProvider = ({
   children,
   ...props
-}: ThemeProviderProps) {
-  return (
-    <NextThemesProvider {...props}>{children}</NextThemesProvider>
-  );
-}
+}: ThemeProviderProps) => (
+  <NextThemesProvider {...props}>{children}</NextThemesProvider>
+);
