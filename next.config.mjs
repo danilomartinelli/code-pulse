d6d7bf3db960db8c96c51692dd63a8d40aa32fc3
Danilo Martinelli <danilomartinelli429@gmail.com>
2024-08-09 20:41:28 -0300

    fix: :ambulance: change the path to envs in jiti
    before this change, path was not exists

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 import { fileURLToPath } from 'node:url';
 import createJiti from 'jiti';
-
 const jiti = createJiti(fileURLToPath(import.meta.url));
+
+jiti('./src/lib/env');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
