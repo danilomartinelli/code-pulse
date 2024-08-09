a733027d1d6653cc81ed199917fbd41e52e6bb33
Danilo Martinelli <danilomartinelli429@gmail.com>
2024-08-09 19:33:07 -0300

    refactor: :art: profile Form Component Schema and Data Types
    refactor the schema and generate a type to export

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,6 +1,10 @@
 import { z } from "zod";
 
+// Profile Form Component Schema and Data Types
+
 export const EditUserProfileSchema = z.object({
   email: z.string().email("Required"),
   name: z.string().min(1, "Required"),
 });
+
+export type EditUserProfileData = z.infer<typeof EditUserProfileSchema>;
