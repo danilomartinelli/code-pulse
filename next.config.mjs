231f81a3192be94fc7e9717c5883442338ff7261
Danilo Martinelli <danilomartinelli429@gmail.com>
2024-08-09 18:47:20 -0300

    feat: :lock: install and use t3-oss to protect and verify all secrets/envs
    Using t3-oss with zod to create validation to each variable at environment

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,8 @@
+import { fileURLToPath } from 'node:url';
+import createJiti from 'jiti';
+
+const jiti = createJiti(fileURLToPath(import.meta.url));
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
