19c120dd8ff786f2da1015b34fb0ad3357898def
Danilo Martinelli <danilomartinelli429@gmail.com>
2024-08-09 13:55:00 -0300

    feat: :pencil2: create a function to set the right type in the eventListener
    Create a generic function to get the right type of Event and extends.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,9 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function createEventListener<T extends Event>(
+  handler: (event: T) => void,
+): EventListener {
+  return (event: Event) => handler(event as T);
+}
