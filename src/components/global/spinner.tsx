1f78f75d801367a4b244c1ac5ebb2173ef4fecf8
Danilo Martinelli <danilomartinelli429@gmail.com>
2024-08-09 13:57:49 -0300

    feat: :lipstick: create a spinner component
    uses tailwind and pure html to create a spinner component with CSS

---
+++ src/components/global/spinner.tsx
@@ -0,0 +1,21 @@
+type SpinnerProps = {
+  size?: "small" | "medium" | "large";
+};
+
+const Spinner = ({ size = "medium" }: SpinnerProps) => {
+  const sizeClasses = {
+    small: "h-4 w-4 border-4",
+    medium: "h-8 w-8 border-4",
+    large: "h-12 w-12 border-4",
+  };
+
+  return (
+    <div className="flex justify-center items-center">
+      <div
+        className={`animate-spin rounded-full ${sizeClasses[size]} border-solid border-foreground/20 border-t-foreground`}
+      />
+    </div>
+  );
+};
+
+export default Spinner;
