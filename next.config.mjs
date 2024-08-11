a1bfa6bb51da5bf347f5bbb5c1deb6e7cccce17a
Danilo Martinelli <danilomartinelli429@gmail.com>
2024-08-11 01:15:40 -0300

    build: :lock: allow clerk and ucarecdn assets
    add clerk and ucarecdn to remote patterns in next config file

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,19 @@ const jiti = createJiti(fileURLToPath(import.meta.url));
 jiti('./src/lib/env');
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img.clerk.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'ucarecdn.com',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
