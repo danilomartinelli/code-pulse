ecc8740427a2be1c37c9b9bf2a365952471b572b
Danilo Martinelli <danilomartinelli429@gmail.com>
2024-08-16 07:41:16 -0300

    test: :wrench: uses turbopack in test environment
    The use of turbopack is a temporary solution to this problem.

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,7 +21,12 @@ export default defineConfig({
     },
   ],
   webServer: {
-    command: 'bun run dev',
+    // The --turbo flag is necessary to prevent intermittent hydration errors on the first render.
+    // This approach is based on a solution found in the discussion at https://stackoverflow.com/a/78599258
+    // It helps ensure that the application undergoes the hydration phase properly, addressing an issue found in Next.js 14.
+    // The use of turbopack is a temporary solution to this problem.
+    // It's not used in development because the Console Ninja plugin for VS Code doesn't work with turbopack.
+    command: 'bun run dev -- --turbo',
     port: 3000,
     reuseExistingServer: !process.env.CI,
     env: {
